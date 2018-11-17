c90be597603eb8651e2154eb5e6458baca41bdf0
Nate St. Germain <nstgerma@uoguelph.ca>
2018-11-17 00:48:52 -0500

    Sample functions

--- GP/Problem/FunctionSet.jl
+++ GP/Problem/FunctionSet.jl
@@ -18,6 +18,9 @@ function chooseRand(funcSet::FunctionSet)
     return funcSet.functions[rand(1:length(funcSet.functions))]
 end
 
+#function isFood(board::Board, param1::Function, param2::Function)
+#end
+
 function isFood(param::Int, param1::Int, param2::Int)
 end
 
--- GP/State/TreeNode.jl
+++ GP/State/TreeNode.jl
@@ -4,5 +4,6 @@
 
 abstract type TreeNode end
 
+# Recursively, parse the tree into a function structure that can be called
 function parseTree(root::TreeNode)
 end
